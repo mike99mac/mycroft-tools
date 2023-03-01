74729a1e24a8beb85351f58b68ecd849f052db4d
mike99mac <mike99mac@gmail.com>
2023-03-01 10:13:38 -0500

    update lsenv, install2, start-mycroft.sh

--- usr/local/sbin/start-mycroft.sh
+++ usr/local/sbin/start-mycroft.sh
@@ -7,27 +7,32 @@ ovosStartScript=~/ovos-core/start-mycroft.sh
 if [ ! -x $ovosStartScript ]; then
   echo "ERROR: did not find executable $ovosStartScript - is ovos-core installed?"
   exit 1
-elif [ ${#VIRTUAL_ENV} = 0 ]; then           # not in a venv
-  if [ -f /home/pi/ovos-core/venv/bin/activate ]; then # activate it
-    source /home/pi/ovos-core/venv/bin/activate 
-  else
-    echo "ERROR: you must run Mycroft in a venv"
-  fi
-  exit 1
 fi
 
 # make log files directories tmpfs's 
-cmd="sudo systemd start home-pi-.local-state-mycroft.mount"
+echo "making /var/log/ directory a tmpfs ..." 
+cmd="sudo systemctl start var-log.mount"
 eval $cmd
 rc=$?
 if [ "$rc" != 0 ]; then
   echo "WARNING $cmd returned $rc - proceeding without tmpfs log directory"
 fi
-cmd="sudo systemd start var-log.mount"
+echo "making Mycroft log directory a tmpfs ..." 
+cmd="sudo systemctl start home-pi-.local-state-mycroft.mount"
 eval $cmd
 rc=$?
 if [ "$rc" != 0 ]; then
   echo "WARNING $cmd returned $rc - proceeding without tmpfs log directory"
+fi
+
+if [ ${#VIRTUAL_ENV} = 0 ]; then           # not in a venv
+  if [ -f /home/pi/ovos-core/venv/bin/activate ]; then # activate it
+    echo "Starting venv ..."
+    source /home/pi/ovos-core/venv/bin/activate 
+  else
+    echo "ERROR: you must run Mycroft in a venv"
+    exit 1
+  fi
 fi
 
 # if no args passed, start Mycroft with "all"
