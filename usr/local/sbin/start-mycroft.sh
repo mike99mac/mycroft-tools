974847069e9e42d3a55bbf5e5b403fdc2fb53b4c
mike99mac <mike99mac@gmail.com>
2023-03-08 05:47:31 -0500

    incorporate ovos-core/start-mycroft.sh

--- usr/local/sbin/start-mycroft.sh
+++ usr/local/sbin/start-mycroft.sh
@@ -28,45 +28,158 @@ function mountFStmpfs
   rc=$?
   if [ "$rc" != 0 ]; then
     echo "WARNING $cmd returned $rc - proceeding without tmpfs $desc" 
+  else 
+    echo "$cmd was successful"
   fi
  }                                         # mountFStmpfs()
 
 #+--------------------------------------------------------------------------+
-function startMycroft 
+function mountLogDirs 
 #+--------------------------------------------------------------------------+
  {
   : SOURCE: ${BASH_SOURCE}
   : STACK:  ${FUNCNAME[@]}
 
-  ovosStartScript=~/ovos-core/start-mycroft.sh
-  if [ ! -x $ovosStartScript ]; then
-    echo "ERROR: did not find executable $ovosStartScript - is ovos-core installed?"
-    exit 1
-  fi
-  
-  # verify that we are in a venv
-  if [ ${#VIRTUAL_ENV} = 0 ]; then           # not in a venv
-    echo "ERROR: Mycroft must be run in a venv"
-    exit 1
-  fi  
-  
   # make log files directories tmpfs's to lengthen the life of the micro-SD card
   mountFStmpfs home-$USER-.local-state-mycroft.mount Mycroft log directory
   mountFStmpfs var-log.mount /var/log
-  if [ ! -d /var/log/mpd ]; then 
+  if [ ! -d /var/log/mpd ]; then           # mpd needs a log file to start
     sudo mkdir /var/log/mpd
     sudo touch /var/log/mpd/mpd.log
   fi
+ } 
   
-  # if no args passed, start Mycroft with "all"
-  if [ $# = 0 ]; then
-    arg1=all
-  else
-    arg1=$1
-  fi
-  $ovosStartScript $arg1
- }                                         # startMycroft()
-  
+
+SOURCE="${BASH_SOURCE[0]}"
+script=${0}
+script=${script##*/}
+cd -P "$( dirname "$SOURCE" )"
+DIR="$( pwd )"
+VIRTUALENV_ROOT=${VIRTUALENV_ROOT:-"${DIR}/.venv"}
+
+function help() {
+    echo "${script}:  Mycroft command/service launcher"
+    echo "usage: ${script} [COMMAND] [restart] [params]"
+    echo
+    echo "Services COMMANDs:"
+    echo "  all                      runs core services: bus, audio, skills, voice"
+    echo "  debug                    runs core services, then starts the CLI"
+    echo "  audio                    the audio playback service"
+    echo "  bus                      the messagebus service"
+    echo "  skills                   the skill service"
+    echo "  voice                    voice capture service"
+    echo "  gui                      gui protocol service"
+    echo "  enclosure                mark_1 enclosure service"
+    echo
+    echo "Tool COMMANDs:"
+    echo "  cli                      the Command Line Interface"
+
+    echo "Options:"
+    echo "  restart                  (optional) Force the service to restart if running"
+    echo
+    echo "Examples:"
+    echo "  ${script} all"
+    echo "  ${script} all restart"
+    echo "  ${script} cli"
+
+    exit 1
+}
+
+_module=""
+function name-to-script-path() {
+    case ${1} in
+        "bus")               _module="mycroft.messagebus.service" ;;
+        "skills")            _module="mycroft.skills" ;;
+        "audio")             _module="mycroft.audio" ;;
+        "voice")             _module="mycroft.client.speech" ;;
+        "cli")               _module="mycroft.client.text" ;;
+        "gui")               _module="mycroft.gui" ;;
+        "enclosure")         _module="mycroft.client.enclosure" ;;
+        *)
+            echo "Error: Unknown name '${1}'"
+            exit 1
+    esac
+}
+
+function launch-process() {
+    name-to-script-path ${1}
+    # Launch process in foreground
+    echo "Starting $1"
+    python3 -m ${_module} $_params
+}
+
+function require-process() {
+    # Launch process if not found
+    name-to-script-path ${1}
+    if ! pgrep -f "python3 (.*)-m ${_module}" > /dev/null ; then
+        # Start required process
+        launch-background ${1}
+    fi
+}
+
+function launch-background() {
+    # Check if given module is running and start (or restart if running)
+    name-to-script-path ${1}
+    if pgrep -f "python3 (.*)-m ${_module}" > /dev/null ; then
+        if ($_force_restart) ; then
+            echo "Restarting: ${1}"
+            "${DIR}/stop-mycroft.sh" ${1}
+        else
+            # Already running, no need to restart
+            return
+        fi
+    else
+        echo "Starting background service $1"
+    fi
+
+    # Launch process in background
+    python3 -m ${_module} $_params &
+}
+
+function launch-all() {
+    echo "Starting all mycroft-core services"
+    launch-background bus
+    launch-background skills
+    launch-background audio
+    launch-background voice
+    launch-background enclosure
+}
+
 # main()
-startMycroft $@                            # start Mycroft passing all args
+# verify that we are in a venv
+if [ ${#VIRTUAL_ENV} = 0 ]; then           # not in a venv
+  echo "ERROR: Mycroft must be run in a venv"
+  exit 1
+fi  
   
+mountLogDirs $@                             # mount tmpfs's over log mount tmpfs's over log directories
+  
+_opt=$1
+_force_restart=false
+shift
+if [[ "${1}" == "restart" ]] || [[ "${_opt}" == "restart" ]] ; then
+    _force_restart=true
+    if [[ "${_opt}" == "restart" ]] ; then
+        # Support "start-mycroft.sh restart all" as well as "start-mycroft.sh all restart"
+        _opt=$1
+    fi
+    shift
+fi
+_params=$@
+ 
+case ${_opt} in
+    bus|gui|audio|skills|voice|enclosure)
+        launch-background ${_opt}
+        ;;
+    cli)
+        launch-process cli
+        ;;
+    debug)
+      # launch-all; launch-process cli
+        launch-all; ovos-cli-client
+        ;;
+    *)
+        launch-all
+        ;;
+esac
+
