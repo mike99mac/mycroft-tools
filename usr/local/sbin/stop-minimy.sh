9fd220c9c88b87090a531ace6269521bdd808b7e
mike99mac <mike99mac@gmail.com>
2023-03-24 15:04:29 -0400

    make stop-minimy do the heavy lifting

--- usr/local/sbin/stop-minimy.sh
+++ usr/local/sbin/stop-minimy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 #
-# stop-minimy.sh - call the Python script to stop Minimy 
+# stop-minimy.sh - kill processes with minimy strings in them 
 #
-echo "Stopping Minimy with: $HOME/minimy/stop.py ..."
-$HOME/minimy/stop.py
+echo "Stopping Minimy ..." 
+procs=`ps -ef | egrep 'minimy/skills|skills/system_skills|framework/services|MsgBus|mimic3|skill_system.py' | grep -v grep`
+if [ ${#procs} = 0 ]; then 
+  echo "Minimy does not appear to be running"
+else
+  echo "$procs" | while read nextProc; do
+    pid=`echo $nextProc | awk '{print $2}'`
+    echo "killing process: $nextProc ..."
+    sudo kill $pid
+  done 
+fi
 sleep 2
 lsenv noclear                              # show status but don't clear screen
+
